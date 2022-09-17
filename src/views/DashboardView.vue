0d035e0c48ff5751c39ae87468f0d1996d817a90
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-17 14:55:44 +0300

    Create dashboard view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,20 @@
+<template>
+<!--    <h1>This is Dashboard Page</h1>-->
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+
+export default {
+  name: "Dashboard",
+
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
