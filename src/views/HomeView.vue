5cdf5fb5f68a062a3f95de6b3a845160329009c5
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-18 00:00:04 +0300

    Create home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,130 @@
+<template>
+
+  <v-container fluid>
+  <v-row
+      align="center"
+      justify="center"
+      class="mt-4"
+  >
+    <v-card
+        class="my-2 mx-2 py-2 px-2"
+        max-width="250"
+        max-height="250"
+        min-width="250"
+        min-height="250"
+    >
+      <v-col>
+        <v-img
+            src="../assets/reception.svg"
+            max-width="160"
+            max-height="160"
+            class="mx-auto"
+        >
+        </v-img>
+                <v-card-actions class="justify-center">
+                  <v-btn
+                      depressed
+                      class="mt-2"
+                  >
+                    Reception & Statistics
+                  </v-btn>
+                </v-card-actions>
+      </v-col>
+    </v-card>
+
+    <v-card
+        class="my-2 mx-2 py-2 px-2"
+        max-width="250"
+        max-height="250"
+        min-width="250"
+        min-height="250"
+    >
+      <v-col>
+        <v-img
+            src="../assets/doctors.svg"
+            max-width="160"
+            max-height="160"
+            class="mx-auto"
+        >
+        </v-img>
+        <v-card-actions class="justify-center">
+          <v-btn
+              depressed
+              class="mt-2"
+          >
+            Doctors
+          </v-btn>
+        </v-card-actions>
+      </v-col>
+    </v-card>
+  </v-row>
+
+    <v-row
+        align="center"
+        justify="center"
+    >
+      <v-card
+          class="my-2 mx-2 py-2 px-2"
+          max-width="250"
+          max-height="250"
+          min-width="250"
+          min-height="250"
+      >
+        <v-col>
+          <v-img
+              src="../assets/lab.svg"
+              max-width="160"
+              max-height="160"
+              class="mx-auto"
+          >
+          </v-img>
+          <v-card-actions class="justify-center">
+            <v-btn
+                depressed
+                class="mt-2"
+            >
+              Medical Lab
+            </v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-card>
+
+      <v-card
+          class="my-2 mx-2 py-2 px-2"
+          max-width="250"
+          max-height="250"
+          min-width="250"
+          min-height="250"
+      >
+        <v-col>
+          <v-img
+              src="../assets/pharmacy.svg"
+              max-width="160"
+              max-height="160"
+              class="mx-auto"
+          >
+          </v-img>
+          <v-card-actions class="justify-center">
+            <v-btn
+                depressed
+                class="mt-2"
+            >
+              Pharmacy
+            </v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-card>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: "HomeView.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
