2b4a394d987a85fc8c4b0548cb000e370f3a75c5
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-18 11:13:59 +0300

    Create search page

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,118 @@
 <template>
-<p>This is Search Page</p>
+
+  <v-container>
+    <v-form>
+      <v-card class="px-6">
+        <v-card-title>Search</v-card-title>
+        <v-card-subtitle>You can search on patient record by Name, Phone or Patient ID</v-card-subtitle>
+        <v-card-title class="subtitle-2">Personal Information</v-card-title>
+
+              <v-row dense>
+                <v-col cols="12">
+                  <v-text-field
+                      label="Full Name"
+                      outlined
+                      dense
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row dense>
+                <v-col cols="6">
+                  <v-text-field
+                      label="Phone"
+                      outlined
+                      dense
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <v-text-field
+                      label="Patient ID"
+                      outlined
+                      dense
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+      </v-card>
+
+
+        <v-row dense align="center" justify="center">
+          <v-spacer></v-spacer>
+            <v-btn
+                class="px-8 py-12 mt-6 mx-8 white--text"
+                color="#6200EE"
+            >
+              <v-col>
+                <v-img
+                    src="../assets/search_icon.svg"
+                    width="50px"
+                    height="50px"
+                    class="mx-auto"
+                >
+
+                </v-img>
+                <h2 class="text-capitalize">Search</h2>
+              </v-col>
+            </v-btn>
+        </v-row>
+    </v-form>
+
+      <v-card class="px-6 mt-16">
+        <v-card-title>Search Results</v-card-title>
+        <v-card-subtitle>You can select the matched result and acces patient information or create a new visit</v-card-subtitle>
+        <v-card-title class="subtitle-2">Personal Information</v-card-title>
+        <v-simple-table>
+          <template>
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Date of Birthday</th>
+              <th>Gender</th>
+              <th>Last Visit</th>
+              <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>Mohammed Ali Qlay</td>
+              <td>0781456325</td>
+              <td>16/2/1990</td>
+              <td>Male</td>
+              <td>1/1/2021</td>
+              <td>
+                <v-btn dark>View</v-btn>
+                <v-btn color="#6200EE" class="white--text ml-6">New Visit</v-btn>
+              </td>
+            </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card>
+    <p class="grey--text">Not found ?  You can create a new patient record by clicking on new patient button</p>
+
+    <v-row dense>
+      <v-col>
+        <v-btn
+            class="px-8 py-12 mt-6 mx-8 white--text"
+            color="#FFFFFF"
+        >
+          <v-col>
+            <v-img
+                src="../assets/loading_icon.svg"
+                width="50px"
+                height="50px"
+                class="mx-auto"
+            >
+
+            </v-img>
+            <h2 class="text-capitalize" style="color: #6200EE">New Patient</h2>
+          </v-col>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+
 </template>
 
 <script>
