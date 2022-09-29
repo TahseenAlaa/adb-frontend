e4e18fc9faec3b54d759ecfee330c2ad143c0477
TahseenAlaa <raspoteen7@gmail.com>
2022-09-29 10:49:55 +0300

    Create pharmacy view only

--- src/components/PharmacyCompo.vue
+++ src/components/PharmacyCompo.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>Pharmacy</h1>
+
+  <div class="mt-6">
+    <v-simple-table>
+      <template>
+        <thead>
+        <tr>
+          <th>Batch No.</th>
+          <th>Expire Date</th>
+          <th>Treatment Type</th>
+          <th>Dosage</th>
+          <th>Quantity</th>
+          <th>Notes</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>452</td>
+          <td>16/2/2023</td>
+          <td>Any</td>
+          <td>5</td>
+          <td>700</td>
+          <td>Notes Here</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+
 </template>
 
 <script>
