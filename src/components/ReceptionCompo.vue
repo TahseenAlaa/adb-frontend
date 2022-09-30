9d172ae68782479d0f7c567e9b68c9bc69c21748
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-30 21:40:59 +0300

    Finish view basic patient information

--- src/components/ReceptionCompo.vue
+++ src/components/ReceptionCompo.vue
@@ -5,10 +5,20 @@
     <v-card-title class="subtitle-2">Personal Information</v-card-title>
 
     <v-row dense>
-      <v-col cols="12">
+      <v-col cols="6">
         <v-text-field
             label="Full Name"
             v-model="full_name"
+            outlined
+            dense
+            readonly
+            hint="ReadOnly"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+            label="Last Visit"
+            v-model="last_visit"
             outlined
             dense
             readonly
@@ -63,7 +73,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="educationQualification"
             label="Education Qualification"
             v-model="education_qualification"
             dense
@@ -74,7 +83,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="maritalStatus"
             label="Marital Status"
             v-model="marital_status"
             dense
@@ -98,7 +106,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="smoker"
             label="Smoker"
             v-model="selectedSmoker"
             dense
@@ -109,7 +116,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="drinker"
             label="Drinker"
             v-model="selectedDrinker"
             dense
@@ -123,7 +129,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="familyHistoryOfDM"
             label="Family History of DM"
             v-model="family_history_of_dm"
             dense
@@ -134,7 +139,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="gestationalDM"
             label="Gestational DM"
             v-model="gestational_dm"
             dense
@@ -158,7 +162,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="hypertension"
             label="Hypertension"
             v-model="selectedHypertension"
             dense
@@ -192,7 +195,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="smbg"
             label="SMBG"
             v-model="selectedSmbg"
             dense
@@ -203,7 +205,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="ihd"
             label="IHD"
             v-model="selectedIhd"
             dense
@@ -214,7 +215,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="cva"
             label="CVA"
             v-model="selectedCva"
             dense
@@ -228,7 +228,6 @@
     <v-row dense>
       <v-col cols="6">
         <v-text-field
-            :items="pvd"
             label="PVD"
             v-model="selectedPvd"
             dense
@@ -239,7 +238,6 @@
       </v-col>
       <v-col cols="6">
         <v-text-field
-            :items="neuropathy"
             label="Neuropathy"
             v-model="selectedNeuropathy"
             dense
@@ -310,7 +308,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="retinopathy"
             label="Retinopathy"
             v-model="selectedRetinopathy"
             dense
@@ -321,7 +318,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="nonProliferative"
             label="Non proliferative"
             v-model="selectedNonProliferative"
             dense
@@ -332,7 +328,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="proliferativeDR"
             label="Proliferative DR"
             v-model="selectedProliferativeDR"
             dense
@@ -346,7 +341,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="maculopathy"
             label="Maculopathy"
             v-model="selectedMaculopathy"
             dense
@@ -357,7 +351,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="insulin"
             label="Insulin"
             v-model="selectedInsulin"
             dense
@@ -368,7 +361,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="amputation"
             label="Amputation"
             v-model="selectedAmputation"
             dense
@@ -382,7 +374,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="ed"
             label="ED"
             v-model="selectedEd"
             dense
@@ -393,7 +384,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="nafld"
             label="NAFLD"
             v-model="selectedNafld"
             dense
@@ -404,7 +394,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="dermopathy"
             label="Dermopathy"
             v-model="selectedDermopathy"
             dense
@@ -418,7 +407,6 @@
     <v-row dense>
       <v-col cols="4">
         <v-text-field
-            :items="diabeticFood"
             label="Diabetic Food"
             v-model="diabetic_food"
             dense
@@ -462,7 +450,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="glycemicControl"
             label="Glycemic Control"
             v-model="glycemic_control"
             dense
@@ -473,7 +460,6 @@
       </v-col>
       <v-col cols="4">
         <v-text-field
-            :items="lipidControl"
             label="Lipid Control"
             v-model="lipid_control"
             dense
@@ -567,99 +553,44 @@
 <script>
 export default {
   props: [
-     'full_name',
-     'phone',
-     'date_of_birthday',
-     'occupation',
-     'address',
-     'selectedSmoker',
-     'selectedDrinker',
-     'family_history_of_dm',
-     'gestational_dm',
-     'weight_of_baby_at_birthday',
-     'selectedHypertension',
-     'family_history_of_ihd',
-     'parity',
-     'selectedSmbg',
-     'selectedIhd',
-     'selectedCva',
-     'selectedPvd',
-     'selectedNeuropathy',
-     'weight',
-     'height',
-     'waist_circumference',
-     'bmi',
-     'hip',
-     'selectedRetinopathy',
-     'selectedNonProliferative',
-     'selectedProliferativeDR',
-     'selectedMaculopathy',
-     'selectedInsulin',
-     'selectedAmputation',
-     'selectedEd',
-     'selectedNafld',
-     'selectedDermopathy',
-     'diabetic_food',
-     'date_of_insulin',
-     'duration_of_insulin',
-     'duration_of_dm',
-     'glycemic_control',
-     'lipid_control',
-     'pressure_control',
-     'father_height',
-     'mother_height',
-     'mid_parent_height',
-     'first_a1c',
-     'second_a1c',
-     'source_of_referral',
-      'date_of_insulin',
-      'DateOfInsulinMenu',
-      'activePicker',
-      'date_of_birthday',
-      'menu',
-      'gender',
-      'maritalStatus',
-      'smoker',
-      'drinker',
-      'educationQualification',
-      'familyHistoryOfDM',
-      'gestationalDM',
-      'hypertension',
-      'smbg',
-      'ihd',
-      'cva',
-      'pvd',
-      'neuropathy',
-      'retinopathy',
-      'nonProliferative',
-      'proliferativeDR',
-      'maculopathy',
-      'insulin',
-      'amputation',
-      'ed',
-      'nafld',
-      'dermopathy',
-      'diabeticFood',
-      'glycemicControl',
-      'lipidControl',
       'full_name',
-      'dateOfBirthday',
+      'last_visit',
+      'date_of_birthday',
       'phone',
+      'selectedGender',
       'occupation',
       'education_qualification',
       'marital_status',
       'address',
+      'selectedSmoker',
+      'selectedDrinker',
       'family_history_of_dm',
       'gestational_dm',
       'weight_of_baby_at_birthday',
+      'selectedHypertension',
       'family_history_of_ihd',
       'parity',
+      'selectedSmbg',
+      'selectedIhd',
+      'selectedCva',
+      'selectedPvd',
+      'selectedNeuropathy',
       'weight',
       'height',
       'waist_circumference',
       'bmi',
       'hip',
+      'selectedRetinopathy',
+      'selectedNonProliferative',
+      'selectedProliferativeDR',
+      'selectedMaculopathy',
+      'selectedInsulin',
+      'selectedAmputation',
+      'selectedEd',
+      'selectedNafld',
+      'selectedDermopathy',
       'diabetic_food',
+      'date_of_insulin',
       'duration_of_insulin',
       'duration_of_dm',
       'glycemic_control',
@@ -671,76 +602,7 @@ export default {
       'first_a1c',
       'second_a1c',
       'source_of_referral',
-      'selectedGender',
-      'selectedCva',
-      'selectedPvd',
-      'selectedNeuropathy',
-      'selectedRetinopathy',
-      'selectedNonProliferative',
-      'selectedProliferativeDR',
-      'selectedMaculopathy',
-      'selectedInsulin',
-      'selectedAmputation',
-      'selectedEd',
-      'selectedNafld',
-      'selectedDermopathy',
-      'selectedSmoker',
-      'selectedDrinker',
-      'selectedSmbg',
-      'selectedIhd',
-      'selectedHypertension',
   ],
-
-  data() {
-    return {
-      // 'full_name': this.$props.patient_name,
-      full_name: this.$props.full_name,
-      phone: this.$props.phone,
-      birthday: this.$props.date_of_birthday,
-      occupation: this.$props.occupation,
-      address: this.$props.address,
-      smoker: this.$props.selectedSmoker,
-      drinker: this.$props.selectedDrinker,
-      family_dm: this.$props.family_history_of_dm,
-      gestational_dm: this.$props.gestational_dm,
-      weight_baby: this.$props.weight_of_baby_at_birthday,
-      hypert: this.$props.selectedHypertension,
-      family_ihd: this.$props.family_history_of_ihd,
-      parity: this.$props.parity,
-      smbg: this.$props.selectedSmbg,
-      ihd: this.$props.selectedIhd,
-      cva: this.$props.selectedCva,
-      pvd: this.$props.selectedPvd,
-      neuro: this.$props.selectedNeuropathy,
-      weight: this.$props.weight,
-      height: this.$props.height,
-      wc: this.$props.waist_circumference,
-      bmi: this.$props.bmi,
-      hip: this.$props.hip,
-      retino: this.$props.selectedRetinopathy,
-      nonpro: this.$props.selectedNonProliferative,
-      prolif: this.$props.selectedProliferativeDR,
-      macul: this.$props.selectedMaculopathy,
-      insul: this.$props.selectedInsulin,
-      amput: this.$props.selectedAmputation,
-      ed: this.$props.selectedEd,
-      nafld: this.$props.selectedNafld,
-      dermo: this.$props.selectedDermopathy,
-      dfoot: this.$props.diabetic_food,
-      date_insulin: this.$props.date_of_insulin,
-      duration_insulin: this.$props.duration_of_insulin,
-      duration_dm: this.$props.duration_of_dm,
-      glycemic: this.$props.glycemic_control,
-      lipid: this.$props.lipid_control,
-      pressure: this.$props.pressure_control,
-      f_height: this.$props.father_height,
-      m_height: this.$props.mother_height,
-      mid_height: this.$props.mid_parent_height,
-      fa1c: this.$props.first_a1c,
-      sa2c: this.$props.second_a1c,
-      referral: this.$props.source_of_referral
-    }
-  },
   name: "ReceptionCompo"
 }
 </script>
--- src/views/ViewPanels.vue
+++ src/views/ViewPanels.vue
@@ -9,7 +9,57 @@
       <v-expansion-panel>
         <v-expansion-panel-header><h2>Patient Information</h2></v-expansion-panel-header>
         <v-expansion-panel-content>
-          <ReceptionCompo :selectedGender="this.patient_uuid"></ReceptionCompo>
+          <ReceptionCompo
+              :full_name="this.full_name"
+              :last_visit="this.last_visit"
+              :date_of_birthday="this.date_of_birthday"
+              :phone="this.phone"
+              :selectedGender="this.selectedGender"
+              :occupation="this.occupation"
+              :education_qualification="this.education_qualification"
+              :marital_status="this.marital_status"
+              :address="this.address"
+              :selectedSmoker="this.selectedSmoker"
+              :selectedDrinker="this.selectedDrinker"
+              :family_history_of_dm="this.family_history_of_dm"
+              :gestational_dm="this.gestational_dm"
+              :weight_of_baby_at_birthday="this.weight_of_baby_at_birthday"
+              :selectedHypertension="this.selectedHypertension"
+              :family_history_of_ihd="this.family_history_of_ihd"
+              :parity="this.parity"
+              :selectedSmbg="this.selectedSmbg"
+              :selectedIhd="this.selectedIhd"
+              :selectedCva="this.selectedCva"
+              :selectedPvd="this.selectedPvd"
+              :selectedNeuropathy="this.selectedNeuropathy"
+              :weight="this.weight"
+              :height="this.height"
+              :waist_circumference="this.waist_circumference"
+              :bmi="this.bmi"
+              :hip="this.hip"
+              :selectedRetinopathy="this.selectedRetinopathy"
+              :selectedNonProliferative="this.selectedNonProliferative"
+              :selectedProliferativeDR="this.selectedProliferativeDR"
+              :selectedMaculopathy="this.selectedMaculopathy"
+              :selectedInsulin="this.selectedInsulin"
+              :selectedAmputation="this.selectedAmputation"
+              :selectedEd="this.selectedEd"
+              :selectedNafld="this.selectedNafld"
+              :selectedDermopathy="this.selectedDermopathy"
+              :diabetic_food="this.diabetic_food"
+              :date_of_insulin="this.date_of_insulin"
+              :duration_of_insulin="this.duration_of_insulin"
+              :duration_of_dm="this.duration_of_dm"
+              :glycemic_control="this.glycemic_control"
+              :lipid_control="this.lipid_control"
+              :pressure_control="this.pressure_control"
+              :father_height="this.father_height"
+              :mother_height="this.mother_height"
+              :mid_parent_height="this.mid_parent_height"
+              :first_a1c="this.first_a1c"
+              :second_a1c="this.second_a1c"
+              :source_of_referral="this.source_of_referral"
+          ></ReceptionCompo>
         </v-expansion-panel-content>
       </v-expansion-panel>
 
@@ -43,14 +93,64 @@ import ReceptionCompo from "@/components/ReceptionCompo";
 import DoctorCompo from "@/components/DoctorCompo";
 import LabCompo from "@/components/LabCompo";
 import PharmacyCompo from "@/components/PharmacyCompo";
+import axios from "axios";
 
 
 export default {
   name: "ViewPanels",
   data() {
     return {
-      'patient_uuid': this.$route.params.patient_uuid,
+      patient_uuid: this.$route.params.patient_uuid,
       autoOpenPanel: [0],
+      full_name: null,
+      last_visit: null,
+      date_of_birthday: null,
+      phone: null,
+      selectedGender: null,
+      occupation: null,
+      education_qualification: null,
+      marital_status: null,
+      address: null,
+      selectedSmoker: null,
+      selectedDrinker: null,
+      family_history_of_dm: null,
+      gestational_dm: null,
+      weight_of_baby_at_birthday: null,
+      selectedHypertension: null,
+      family_history_of_ihd: null,
+      parity: null,
+      selectedSmbg: null,
+      selectedIhd: null,
+      selectedCva: null,
+      selectedPvd: null,
+      selectedNeuropathy: null,
+      weight: null,
+      height: null,
+      waist_circumference: null,
+      bmi: null,
+      hip: null,
+      selectedRetinopathy: null,
+      selectedNonProliferative: null,
+      selectedProliferativeDR: null,
+      selectedMaculopathy: null,
+      selectedInsulin: null,
+      selectedAmputation: null,
+      selectedEd: null,
+      selectedNafld: null,
+      selectedDermopathy: null,
+      diabetic_food: null,
+      date_of_insulin: null,
+      duration_of_insulin: null,
+      duration_of_dm: null,
+      glycemic_control: null,
+      lipid_control: null,
+      pressure_control: null,
+      father_height: null,
+      mother_height: null,
+      mid_parent_height: null,
+      first_a1c: null,
+      second_a1c: null,
+      source_of_referral: null,
     }
   },
   components: {
@@ -58,6 +158,70 @@ export default {
     DoctorCompo,
     LabCompo,
     PharmacyCompo
+  },
+
+  created() {
+    let baseURL = this.$store.getters.baseURL
+
+    axios.get(baseURL + 'api/v1/patients/' + this.patient_uuid, {
+      headers: {
+        'Content-Type' : 'application/json',
+        'Accept'       : 'application/json',
+        'Authorization': 'Bearer '+localStorage.getItem('esite_token')
+      }
+    }).then(({data})=>{
+      this.full_name = data.patient_info.full_name
+      this.last_visit = data.patient_info.last_visit
+      this.date_of_birthday = data.patient_info.birthdate
+      this.phone = data.patient_info.phone
+      this.selectedGender = data.patient_info.gender
+      this.occupation = data.patient_latest_history.occupation
+      this.education_qualification = data.patient_latest_history.education_qualification
+      this.marital_status = data.patient_latest_history.marital_status
+      this.address = data.patient_latest_history.address
+      this.selectedSmoker = data.patient_latest_history.smoker
+      this.selectedDrinker = data.patient_latest_history.drinker
+      this.family_history_of_dm = data.patient_latest_history.family_dm
+      this.gestational_dm = data.patient_latest_history.gestational_dm
+      this.weight_of_baby_at_birthday = data.patient_latest_history.weight_baby
+      this.selectedHypertension = data.patient_latest_history.hypert
+      this.family_history_of_ihd = data.patient_latest_history.family_ihd
+      this.parity = data.patient_latest_history.parity
+      this.selectedSmbg = data.patient_latest_history.smbg
+      this.selectedIhd = data.patient_latest_history.ihd
+      this.selectedCva = data.patient_latest_history.cva
+      this.selectedPvd = data.patient_latest_history.pvd
+      this.selectedNeuropathy = data.patient_latest_history.neuro
+      this.weight = data.patient_latest_history.weight
+      this.height = data.patient_latest_history.height
+      this.waist_circumference = data.patient_latest_history.wc
+      this.bmi = data.patient_latest_history.bmi
+      this.hip = data.patient_latest_history.hip
+      this.selectedRetinopathy = data.patient_latest_history.retino
+      this.selectedNonProliferative = data.patient_latest_history.nonpro
+      this.selectedProliferativeDR = data.patient_latest_history.prolif
+      this.selectedMaculopathy = data.patient_latest_history.macul
+      this.selectedInsulin = data.patient_latest_history.insul
+      this.selectedAmputation = data.patient_latest_history.amput
+      this.selectedEd = data.patient_latest_history.ed
+      this.selectedNafld = data.patient_latest_history.nafld
+      this.selectedDermopathy = data.patient_latest_history.dermo
+      this.diabetic_food = data.patient_latest_history.dfoot
+      this.date_of_insulin = data.patient_latest_history.date_insulin
+      this.duration_of_insulin = data.patient_latest_history.duration_insulin
+      this.duration_of_dm = data.patient_latest_history.duration_dm
+      this.glycemic_control = data.patient_latest_history.glycemic
+      this.lipid_control = data.patient_latest_history.lipid
+      this.pressure_control = data.patient_latest_history.pressure
+      this.father_height = data.patient_latest_history.f_height
+      this.mother_height = data.patient_latest_history.m_height
+      this.mid_parent_height = data.patient_latest_history.mid_height
+      this.first_a1c = data.patient_latest_history.fa1c
+      this.second_a1c = data.patient_latest_history.sa2c
+      this.source_of_referral = data.patient_latest_history.referral
+    }).catch(({response:{data}})=>{
+      console.log(data)
+    });
   }
 }
 </script>
