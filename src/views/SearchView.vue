107c84550591608bd3c303987329ab49c5ab3aeb
Tahseen Alaa <raspoteen7@gmail.com>
2022-10-15 02:40:49 +0300

    Create not found in search page

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -60,7 +60,39 @@
         </v-row>
     </v-form>
 
-      <v-card class="px-6 mt-16"
+<!--      START No Result Alert-->
+    <div v-if="toggles.showNoResultAlert">
+      <v-row dense>
+        <v-spacer></v-spacer>
+        <v-col cols="4">
+          <img src="../assets/undraw_file_searching_re_3evy.svg" alt="" width="301" height="315">
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+
+      <v-alert
+          prominent
+          type="error"
+          class="mt-12"
+      >
+        <v-row align="center">
+          <v-col class="grow">Unfortunately, No Results Found!</v-col>
+          <v-col class="shrink">
+                <router-link to="/reception">
+                  <v-btn
+                      class="px-2 py-6 deep-purple white--text"
+                  >
+                    <v-icon size="40" dark>mdi-folder-plus</v-icon>
+                    <h3 class="text-capitalize">New Patient</h3>
+                  </v-btn>
+                </router-link>
+          </v-col>
+        </v-row>
+      </v-alert>
+    </div>
+<!--      END No Result Alert-->
+
+    <v-card class="px-6 mt-16"
               v-if="toggles.showResultsPanel"
       >
         <v-card-title>Search Results</v-card-title>
@@ -142,17 +174,27 @@
         v-if="toggles.showResultsPanel === true && receptionTeam === true"
         dense
     >
-      <v-col>
-        <router-link to="/reception">
-          <v-btn
-              class="px-2 py-12 mt-6 mx-2 white deep-purple--text"
-          >
-            <v-col>
-              <v-icon size="60">mdi-folder-plus</v-icon>
-              <h3 class="text-capitalize">New Patient</h3>
+      <v-col cols="12">
+        <v-alert
+            prominent
+            color="#2A3B4D"
+            dark
+            class="mt-12"
+        >
+          <v-row align="center">
+            <v-col class="grow">No Results Found?</v-col>
+            <v-col class="shrink">
+              <router-link to="/reception">
+                <v-btn
+                    class="px-2 py-6 deep-purple white--text"
+                >
+                  <v-icon size="40" dark>mdi-folder-plus</v-icon>
+                  <h3 class="text-capitalize">New Patient</h3>
+                </v-btn>
+              </router-link>
             </v-col>
-          </v-btn>
-        </router-link>
+          </v-row>
+        </v-alert>
       </v-col>
     </v-row>
   </v-container>
@@ -179,7 +221,8 @@ export default {
       department: this.$route.params.department,
       toggles:{
         showResultsPanel: false,
-        showNewVisit: false
+        showNewVisit: false,
+        showNoResultAlert: false
       },
     }
   },
@@ -190,8 +233,12 @@ export default {
         httpPOST('api/v1/patients/search-by-patient-id/' + this.patient_id)
             .then(({data})=>{
           this.search_result = data
-          if (data) {
+          if (data.data.length > 0) {
             this.toggles.showResultsPanel = true
+            this.toggles.showNoResultAlert = false
+          } else {
+            this.toggles.showResultsPanel = false
+            this.toggles.showNoResultAlert = true
           }
         }).catch(({response:{data}})=>{
           console.log(data)
