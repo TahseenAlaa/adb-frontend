631df2624c1e1bdb32a1849417ca536a34e99a39
Tahseen Alaa <raspoteen7@gmail.com>
2022-10-19 08:28:42 +0300

    Create basic permissions view

--- src/router/index.js
+++ src/router/index.js
@@ -76,7 +76,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/permissions',
+    name: 'permissions',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PermissionsView.vue')
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/PermissionsView.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+      <li v-for="p in permissions_list">
+        {{ p.name }}
+      </li>
+    </ul>
+
+</template>
+
+<script>
+import {httpGET} from "@/utils/utils";
+
+export default {
+  name: "PermissionsView.vue",
+  data() {
+    return {
+      permissions_list: [],
+    }
+  },
+  created() {
+    httpGET('api/v1/permissions/index')
+        .then(({data}) => {
+          this.permissions_list = data.data
+          console.log(data.data)
+        }).catch(({response:{data}})=>{
+          console.log(data)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
