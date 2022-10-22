55921bab58b21c02463ff475e0d92abbd9bd144a
TahseenAlaa <raspoteen7@gmail.com>
2022-10-22 23:10:41 +0300

    Delete old info in view panels of Lab doctor and phrmacy

--- src/components/PharmacyCompo.vue
+++ src/components/PharmacyCompo.vue
@@ -1,43 +1,12 @@
 <template>
-
-  <div class="mt-6">
-    <v-simple-table>
-      <template>
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>Batch No.</th>
-          <th>Expire Date</th>
-          <th>Treatment Type</th>
-          <th>Dosage</th>
-          <th>Quantity</th>
-          <th>Notes</th>
-          <th>Tester Name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="drug in drugsList">
-          <td>{{ drug.name }}</td>
-          <td>{{ drug.batch_no }}</td>
-          <td>{{ drug.expire_date }}</td>
-          <td>{{ drug.treatment_type }}</td>
-          <td>{{ drug.dosage }}</td>
-          <td>{{ drug.quantity }}</td>
-          <td>{{ drug.notes }}</td>
-          <td>{{ drug.created_by }}</td>
-        </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-  </div>
-
+  <h1>Pharmacy Section</h1>
 </template>
 
 <script>
 export default {
   name: "PharmacyCompo",
   props: [
-      'drugsList'
+    'patient_uuid'
   ],
 
   methods: {
