ced689ed44698cd4be533917fee5d439e90684aa
TahseenAlaa <raspoteen7@gmail.com>
2022-10-31 16:45:01 +0300

    Create simple data table in the inventory

--- src/views/InventoryManagementView.vue
+++ src/views/InventoryManagementView.vue
@@ -28,11 +28,103 @@
             value="tab-input"
         >
           <v-card flat>
-            <v-card-text>input Tab</v-card-text>
+            <v-card-text>
+              <v-row dense>
+                <v-col cols="2" class="my-4">
+                  <v-btn
+                      color="deep-purple white--text"
+                      class="px-2 py-5 mx-2"
+                  >
+                    <v-icon size="30" class="pr-1">mdi-archive-arrow-down-outline</v-icon>
+                    New Document
+                  </v-btn>
+                </v-col>
+              </v-row>
+
+              <v-simple-table dense>
+                <template v-slot:default>
+                  <thead>
+                  <tr>
+                    <th class="text-left">Source</th>
+                    <th class="text-left">Destination</th>
+                    <th class="text-left">Source Name</th>
+                    <th class="text-left">Destination Name</th>
+                    <th class="text-left">Created At</th>
+                    <th class="text-left">Action</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>
+                      <v-btn
+                          x-small
+                          color="teal darken-1"
+                          dark
+                          class="px-1 mx-1"
+                          @click=""
+                      >
+                        <v-icon size="20" class="pr-1">mdi-lead-pencil</v-icon>
+                        Edit
+                      </v-btn>
+                      <v-btn
+                          x-small
+                          color="deep-orange darken-1 white--text"
+                          class="px-1 mx-1"
+                          @click="activeDeleteDialog(test.id)"
+                          :disabled="disableDeleteBTN"
+                      >
+                        <v-icon size="20" class="pr-1">mdi-delete-forever</v-icon>
+                        Delete
+                      </v-btn>
+                    </td>
+                  </tr>
+                  <!--              START Delete Dialog -->
+                  <v-row justify="center">
+                    <v-dialog
+                        persistent
+                        max-width="230"
+                    >
+                      <v-card>
+                        <v-card-title class="text-h5">
+                          Delete Test
+                        </v-card-title>
+                        <v-card-text class="text-center">
+                          Are you sure to delete this Test?
+                        </v-card-text>
+                        <v-card-actions class="d-flex justify-center">
+                          <v-btn
+                              dark
+                              class="deep-grey"
+                          >
+                            Close
+                          </v-btn>
+                          <v-btn
+                              color="deep-orange darken-1"
+                              dark
+                              class="px-1 mx-1"
+                          >
+                            <v-icon size="30" class="pr-1">mdi-delete-forever</v-icon>
+                            Delete
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </v-row>
+                  <!--              END Delete Dialog -->
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card-text>
           </v-card>
         </v-tab-item>
       </v-tabs-items>
 <!--      END Input Documents -->
+
 
 <!--      START Output Documents -->
       <v-tabs-items v-model="tab">
@@ -40,7 +132,98 @@
             value="tab-output"
         >
           <v-card flat>
-            <v-card-text>Output Tab</v-card-text>
+            <v-card-text>
+              <v-row dense>
+                <v-col cols="2" class="my-4">
+                  <v-btn
+                      color="deep-purple white--text"
+                      class="px-2 py-5 mx-2"
+                  >
+                    <v-icon size="30" class="pr-1">mdi-archive-arrow-down-outline</v-icon>
+                    New Document
+                  </v-btn>
+                </v-col>
+              </v-row>
+
+              <v-simple-table dense>
+                <template v-slot:default>
+                  <thead>
+                  <tr>
+                    <th class="text-left">Source</th>
+                    <th class="text-left">Destination</th>
+                    <th class="text-left">Source Name</th>
+                    <th class="text-left">Destination Name</th>
+                    <th class="text-left">Created At</th>
+                    <th class="text-left">Action</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>
+                      <v-btn
+                          x-small
+                          color="teal darken-1"
+                          dark
+                          class="px-1 mx-1"
+                          @click=""
+                      >
+                        <v-icon size="20" class="pr-1">mdi-lead-pencil</v-icon>
+                        Edit
+                      </v-btn>
+                      <v-btn
+                          x-small
+                          color="deep-orange darken-1 white--text"
+                          class="px-1 mx-1"
+                          @click="activeDeleteDialog(test.id)"
+                          :disabled="disableDeleteBTN"
+                      >
+                        <v-icon size="20" class="pr-1">mdi-delete-forever</v-icon>
+                        Delete
+                      </v-btn>
+                    </td>
+                  </tr>
+                  <!--              START Delete Dialog -->
+                  <v-row justify="center">
+                    <v-dialog
+                        persistent
+                        max-width="230"
+                    >
+                      <v-card>
+                        <v-card-title class="text-h5">
+                          Delete Test
+                        </v-card-title>
+                        <v-card-text class="text-center">
+                          Are you sure to delete this Test?
+                        </v-card-text>
+                        <v-card-actions class="d-flex justify-center">
+                          <v-btn
+                              dark
+                              class="deep-grey"
+                          >
+                            Close
+                          </v-btn>
+                          <v-btn
+                              color="deep-orange darken-1"
+                              dark
+                              class="px-1 mx-1"
+                          >
+                            <v-icon size="30" class="pr-1">mdi-delete-forever</v-icon>
+                            Delete
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </v-row>
+                  <!--              END Delete Dialog -->
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card-text>
           </v-card>
         </v-tab-item>
       </v-tabs-items>
