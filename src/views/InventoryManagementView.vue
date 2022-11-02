5dd0ee3a8daee593738839a34475d1f6d49891dd
TahseenAlaa <raspoteen7@gmail.com>
2022-11-02 19:03:04 +0300

    Fix bugs in input documents

--- src/views/InventoryManagementView.vue
+++ src/views/InventoryManagementView.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <v-container>
+  <v-container class="mb-12 pb-12">
 
     <v-card>
 <!--      START Input Documents -->
@@ -50,43 +50,48 @@
                 <template v-slot:default>
                   <thead>
                   <tr>
-                    <th class="text-left">Source</th>
-                    <th class="text-left">Destination</th>
                     <th class="text-left">Source Name</th>
+                    <th class="text-left">Source Reference</th>
                     <th class="text-left">Destination Name</th>
+                    <th class="text-left">Destination Reference</th>
+                    <th class="text-left">Approved By</th>
                     <th class="text-left">Created At</th>
+                    <th class="text-left">Created By</th>
                     <th class="text-left">Action</th>
                   </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>
-                      <v-btn
-                          x-small
-                          color="teal darken-1"
-                          dark
-                          class="px-1 mx-1"
-                          @click=""
-                      >
-                        <v-icon size="20" class="pr-1">mdi-lead-pencil</v-icon>
-                        Edit
-                      </v-btn>
-                      <v-btn
-                          x-small
-                          color="deep-orange darken-1 white--text"
-                          class="px-1 mx-1"
-                          @click="activeDeleteDialog(test.id)"
-                          :disabled="disableDeleteBTN"
-                      >
-                        <v-icon size="20" class="pr-1">mdi-delete-forever</v-icon>
-                        Delete
-                      </v-btn>
-                    </td>
+                  <tr v-for="item in inventory">
+                    <td>{{ item.source_name }}</td>
+                    <td>{{ item.source_ref }}</td>
+                    <td>{{ item.destination_name }}</td>
+                    <td>{{ item.destination_ref }}</td>
+                    <td>{{ item.approved_by }}</td>
+                    <td>{{ humanReadableDateConverter(item.created_at) }}</td>
+                    <td>{{ item.updated_user? item.updated_user.full_name : item.user.full_name }}</td>
+                    <td></td>
+<!--                    <td>-->
+<!--                      <v-btn-->
+<!--                          x-small-->
+<!--                          color="teal darken-1"-->
+<!--                          dark-->
+<!--                          class="px-1 mx-1"-->
+<!--                          @click=""-->
+<!--                      >-->
+<!--                        <v-icon size="20" class="pr-1">mdi-lead-pencil</v-icon>-->
+<!--                        Edit-->
+<!--                      </v-btn>-->
+<!--                      <v-btn-->
+<!--                          x-small-->
+<!--                          color="deep-orange darken-1 white&#45;&#45;text"-->
+<!--                          class="px-1 mx-1"-->
+<!--                          @click="activeDeleteDialog(test.id)"-->
+<!--                          :disabled="disableDeleteBTN"-->
+<!--                      >-->
+<!--                        <v-icon size="20" class="pr-1">mdi-delete-forever</v-icon>-->
+<!--                        Delete-->
+<!--                      </v-btn>-->
+<!--                    </td>-->
                   </tr>
                   <!--              START Delete Dialog -->
                   <v-row justify="center">
@@ -242,45 +247,61 @@
 
 
 
-
+<LoadingDialogCompo :loading_-dialog="dialog.loading.active"></LoadingDialogCompo>
   </v-container>
 
 </template>
 
 <script>
+import {httpGET} from "@/utils/utils";
+import LoadingDialogCompo from "@/components/LoadingDialogCompo";
+
 export default {
   name: "InventoryManagementView.vue",
+  components: {
+    LoadingDialogCompo
+  },
+
   data() {
     return {
-      inputs: [{
-        name: '',
-        party: ''
-      }],
+      inventory: [],
       tab: null,
+      dialog: {
+        loading: {
+          active: false
+        }
+      }
     }
   },
 
   methods: {
-    add () {
-      this.inputs.push({
-        name: '',
-        party: ''
-      })
-      console.log(JSON.stringify(this.inputs))
+    humanReadableDateConverter (date) {
+      if (date) {
+        let newDate = new Date(date)
+        return newDate.toLocaleDateString('en-GB')
+      } else {
+        return null
+      }
     },
+  },
 
-    remove (index) {
-      this.inputs.splice(index, 1)
-    },
+  mounted() {
+    this.dialog.loading.active = true
+  },
 
-    addCandidate () {
-      axios
-          .post('/candidates', {
-            my_prop_name: JSON.stringify(this.inputs)
-          })
-          .then(response => {})
-          .catch(error => {})
-    },
+  created() {
+    // START Fetch Documents List
+    httpGET('api/v1/documents/index-inventory')
+        .then(({data}) => {
+          this.inventory = data.data
+        })
+        .catch(({response:{data}})=>{
+          console.log(data)
+        })
+        .finally(() => {
+          this.dialog.loading.active = false
+        });
+    // END Fetch Documents List
   }
 }
 </script>
