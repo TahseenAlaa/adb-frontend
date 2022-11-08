84847c66e3c8453a7b324fbacfbdcf9f49f8b367
TahseenAlaa <raspoteen7@gmail.com>
2022-11-08 11:30:08 +0300

    Create new provider function

--- src/views/ProvidersView.vue
+++ src/views/ProvidersView.vue
@@ -2,6 +2,21 @@
   <v-container>
     <v-card class="px-6 pb-12 mb-12">
       <v-card-title>Providers Management</v-card-title>
+      <v-card-subtitle>
+        <v-row dense>
+          <v-spacer></v-spacer>
+          <v-col cols="2" class="my-4">
+            <v-btn
+                color="deep-purple white--text"
+                class="px-2 py-5 mx-2"
+                @click="newProviderDialogActive"
+            >
+              <v-icon size="30" class="pr-1">mdi-rss-box</v-icon>
+              New Provider
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-subtitle>
       <v-simple-table dense>
         <template v-slot:default>
           <thead>
@@ -84,6 +99,59 @@
           </tbody>
         </template>
       </v-simple-table>
+
+      <!--    START New providers Dialog -->
+      <div>
+        <v-col>
+          <v-dialog
+              v-model="provider.new.dialog"
+              max-width="800px"
+          >
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">Add New Provider</span>
+              </v-card-title>
+              <v-card-subtitle class="subtitle-1">Please fill the information below to add a new provider item.</v-card-subtitle>
+              <v-card-text>
+                <v-card-subtitle class="subtitle-2">Provider Information</v-card-subtitle>
+                <v-container>
+                  <v-row dense>
+                    <v-col
+                        cols="12"
+                    >
+                      <v-text-field
+                          label="Provider Name"
+                          v-model="provider.new.name"
+                          outlined
+                          dense
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    class="deep-purple white--text"
+                    text
+                    @click="provider.new.dialog = false"
+                >
+                  Close
+                </v-btn>
+                <v-btn
+                    class="deep-purple white--text"
+                    text
+                    @click="postNewProviderData"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </div>
+      <!--    END New providers Dialog -->
+
     </v-card>
     <!--    START Loading Dialog-->
     <LoadingDialogCompo :loading_-dialog="loading_Dialog"></LoadingDialogCompo>
@@ -94,7 +162,7 @@
 
 <script>
 import LoadingDialogCompo from "@/components/LoadingDialogCompo";
-import {httpGET} from "@/utils/utils";
+import {httpGET, httpPOST} from "@/utils/utils";
 
 export default {
   name: "ProvidersView.vue",
@@ -105,7 +173,15 @@ export default {
     return {
       loading_Dialog: true,
       providers: [],
-      disableSaveBTN: false
+      disableSaveBTN: false,
+      provider: {
+        new: {
+          dialog: false,
+          name: null,
+          drug_type: null,
+          item_type: null
+        }
+      }
     }
   },
 
@@ -121,6 +197,27 @@ export default {
       } else {
         return null
       }
+    },
+
+    newProviderDialogActive() {
+      this.provider.new.dialog = true
+    },
+
+    postNewProviderData() {
+      this.loading_state = true
+
+      httpPOST('api/v1/providers/store', {
+        name: this.provider.new.name
+      })
+          .then(({data}) => {
+            this.providers = data.data
+            this.provider.new.dialog = false
+
+            this.provider.new.name = null
+          }).catch(({response:{data}})=>{
+        console.log(data)
+      });
+      this.loading_state = false
     },
   },
 
