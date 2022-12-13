472606f756f2bed13c46e17cafd094b132b2baec
TahseenAlaa <raspoteen7@gmail.com>
2022-12-13 14:59:20 +0300

    Add Sentry

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
 Vue.config.productionTip = false
 
@@ -12,3 +14,18 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+Sentry.init({
+  Vue,
+  dsn: "https://148275e20e68417fa98ff915bc83ea1c@o4504321553530880.ingest.sentry.io/4504321556480001",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
