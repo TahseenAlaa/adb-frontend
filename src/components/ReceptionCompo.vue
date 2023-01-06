5229faaa937234e02a8899bbf1c07bbbf28e1b4d
TahseenAlaa <raspoteen7@gmail.com>
2023-01-06 18:35:21 +0300

    Remove white spaces from old patient number

--- src/components/ReceptionCompo.vue
+++ src/components/ReceptionCompo.vue
@@ -890,7 +890,7 @@ export default {
         family_history_of_ihd: this.family_history_of_ihd,
         weight_of_baby_at_birthday: this.weight_of_baby_at_birthday,
         date_of_dm: this.date_of_dm,
-        patient_number: this.patient_number,
+        patient_number: this.patient_number.replaceAll(/\s/g,''),
         social_status: this.social_status,
       })
           .then(({data}) => {
